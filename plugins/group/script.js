6c16c27307a9890eb345a1f10c5cf09b0004e191
elzoz-2010 <hmdawalnwb@gmail.com>
2026-05-12 07:01:23 +0300

    Update script.js

--- plugins/group/script.js
+++ plugins/group/script.js
@@ -7,8 +7,8 @@ const context = (jid, img) => ({
     isForwarded: true,
     forwardingScore: 1,
     forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363225356834044@newsletter',
-        newsletterName: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+        newsletterJid: '120363407772474123@newsletter',
+        newsletterName: '𝐸𝐿𝑍𝑂𝑍  ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
         serverMessageId: 0
     },
     externalAdReply: {
