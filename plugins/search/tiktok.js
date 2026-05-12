5a29ac26abe9630c820c4344ae5192db456bbdef
elzoz-2010 <hmdawalnwb@gmail.com>
2026-05-12 07:08:40 +0300

    Update tiktok.js

--- plugins/search/tiktok.js
+++ plugins/search/tiktok.js
@@ -22,8 +22,8 @@ if (data && data.length > 0) {
         ],
         mentions: [m.sender],
         newsletter: {
-            name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
-            jid: '120363225356834044@newsletter'
+            name: '𝐸𝐿𝑍𝑂𝑍 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+            jid: '120363407772474123@newsletter'
         },
     }, global.reply_status)
 } else {
