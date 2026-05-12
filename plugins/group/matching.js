287789a1a20e36a9c27c6e239aaf9fd8b825e517
elzoz-2010 <hmdawalnwb@gmail.com>
2026-05-12 07:00:28 +0300

    Update matching.js

--- plugins/group/matching.js
+++ plugins/group/matching.js
@@ -14,8 +14,8 @@ const { data } = JSON.parse(res);
   ], 
   mentions: [m.sender],
   newsletter: {
-      name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
-      jid: '120363225356834044@newsletter'
+      name: '𝐸𝐿𝑍𝑂𝑍 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+      jid: '120363407772474123@newsletter'
     },
 }, global.reply_status)
 
@@ -32,8 +32,8 @@ return await conn.sendButtonNormal(m.chat, {
   ], 
   mentions: [m.sender],
   newsletter: {
-      name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
-      jid: '120363225356834044@newsletter'
+      name: '𝐸𝐿𝑍𝑂𝑍 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+      jid: '120363407772474123@newsletter'
     },
 }, global.reply_status)
 
