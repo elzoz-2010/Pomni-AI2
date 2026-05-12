13680552b9c6b99757ce4f5842ee49e0de485c4b
elzoz-2010 <hmdawalnwb@gmail.com>
2026-05-12 06:39:21 +0300

    Update settings.js

--- plugins/admins/settings.js
+++ plugins/admins/settings.js
@@ -39,7 +39,7 @@ async function handler(m, { conn, command, args }) {
     if (!subCmd) {
         await conn.sendButton(m.chat, {
             bodyText:  menu,
-            footerText: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️",
+            footerText: "𝐸𝐿𝑍𝑂𝑍 𝑒𝑙𝑚𝑜𝑠ℎ𝑎𝑔ℎ𝑖𝑏 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️",
             buttons: [
     { name: "quick_reply", params: { display_text: "🪐 ايقاف التنصيب (البوتات الفرعي)", id: ".تفعيل ايقاف_الفرعي" } },
     { name: "quick_reply", params: { display_text: "🚀 تشغيل التنصيب", id: ".تفعيل تشغيل_الفرعي" } },
@@ -56,8 +56,8 @@ async function handler(m, { conn, command, args }) {
 ],
           mentions: [m.sender],
   newsletter: {
-      name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
-      jid: '120363225356834044@newsletter'
+      name: '𝐸𝐿𝑍𝑂𝑍 𝑒𝑙𝑚𝑜𝑠ℎ𝑎𝑔ℎ𝑖𝑏 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+      jid: '120363407772474123@newsletter'
     },
   interactiveConfig: {
     buttons_limits: 1, // لازم تبقي واحد
