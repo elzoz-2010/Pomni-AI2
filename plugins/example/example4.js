bbb7c00d94236ac841cde35698894b81385eeeb9
elzoz-2010 <hmdawalnwb@gmail.com>
2026-05-12 06:54:45 +0300

    Update example4.js

--- plugins/example/example4.js
+++ plugins/example/example4.js
@@ -54,8 +54,8 @@ await conn.sendCarousel(m.chat, {
   ],
   mentions: [m.sender],
   newsletter: {
-      name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
-      jid: '120363225356834044@newsletter'
+      name: '𝐸𝐿𝑍𝑂𝑍 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+      jid: '120363407772474123@newsletter'
     },
 }, m)
 
